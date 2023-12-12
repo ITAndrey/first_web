91cbdeeee3e22e950f0bfd0e73cd0c91e0287c2e
Tupikov Andrey <dimadokks@gmail.com>
2023-12-12 23:57:25 +0500

    QuarySet, basket and decorators

--- store/products/models.py
+++ store/products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -13,9 +15,32 @@ class Products(models.Model):
     name = models.CharField(max_length=256)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products_images')
+    image = models.ImageField(upload_to="products_images")
     categories = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Продукт: {self.name} | Категория: {self.categories.name}"
+
+
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Products, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects=BasketQuerySet.as_manager()
 
 
     def __str__(self):
-        return f'Продукт: {self.name} | Категория: {self.categories.name}'
+        return f"Корзина для: {self.user.username} | Продукт: {self.product.name}"
+
+    def sum(self):
+        return self.product.price * self.quantity
--- store/users/views.py
+++ store/users/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponsePermanentRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from products.models import Basket
 
 
 def login(request):
@@ -34,7 +36,7 @@ def register(request):
     context = {"form": form}
     return render(request, "users/register.html", context)
 
-
+@login_required
 def profile(request):
     if request.method == "POST":
         form = UserProfileForm(
@@ -47,10 +49,15 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
-    context = {"title": "Store - Профиль", "form": form}
+
+    context = {
+        "title": "Store - Профиль",
+        "form": form,
+        "baskets": Basket.objects.filter(user=request.user),
+    }
     return render(request, "users/profile.html", context)
 
 
 def logout(request):
     auth.logout(request)
-    return HttpResponsePermanentRedirect(reverse('index'))
+    return HttpResponsePermanentRedirect(reverse("index"))
